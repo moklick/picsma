f20aa0873f1e2f2f2dfe53afb24b11ad85096251
andymensional <andy.bialystock@gmail.com>
2013-03-01 00:42:17 +0100

    wrapped picsma.js in $()

--- js/picsma.js
+++ js/picsma.js
@@ -1,50 +1,52 @@
-var maxW = 800, maxH = 600;
-var can = $('#canvas')[0], ctx = can.getContext('2d');
-var dropArea = document;
+$(function () {
+    var maxW = 800, maxH = 600;
+    var can = $('#canvas')[0], ctx = can.getContext('2d');
+    var dropArea = document;
 
-dropArea.addEventListener('dragenter', stopDefault, false);
-dropArea.addEventListener('dragover', stopDefault, false);
-dropArea.addEventListener('dragleave', stopDefault, false);
-dropArea.addEventListener('drop', dropHandler, false);
+    dropArea.addEventListener('dragenter', stopDefault, false);
+    dropArea.addEventListener('dragover', stopDefault, false);
+    dropArea.addEventListener('dragleave', stopDefault, false);
+    dropArea.addEventListener('drop', dropHandler, false);
 
-function stopDefault(e) {
-    e.stopPropagation();
-    e.preventDefault();
-}
+    function stopDefault(e) {
+        e.stopPropagation();
+        e.preventDefault();
+    }
 
-function dropHandler(e) {
-    stopDefault(e);
-    var file = e.dataTransfer.files[0];
-    if (!file.type.match(/image.*/)) return;
-    var reader = new FileReader();
-    reader.onerror = function(e) {
-        alert('Error: ' + e.target.error);
-    };
-    reader.onload = (function(aFile) {
-        return function(evt) {
-            var tmpimg = new Image();
-            var newW,newH;
-            tmpimg.src = evt.target.result;
-            tmpimg.onload = function(){
-                if (tmpimg.width> maxW||tmpimg.height> maxH){
-                    var ratioW = tmpimg.width/maxW;
-                    var ratioH = tmpimg.height/maxH;
-                    if (ratioW>ratioH) {
-                        newW = maxW;
-                        newH=tmpimg.height/ratioW;
-                    }else{
-                        newH=maxH;
-                        newW=tmpimg.width/ratioH;
+    function dropHandler(e) {
+        stopDefault(e);
+        var file = e.dataTransfer.files[0];
+        if (!file.type.match(/image.*/)) return;
+        var reader = new FileReader();
+        reader.onerror = function (e) {
+            alert('Error: ' + e.target.error);
+        };
+        reader.onload = (function (aFile) {
+            return function (evt) {
+                var tmpimg = new Image();
+                var newW, newH;
+                tmpimg.src = evt.target.result;
+                tmpimg.onload = function () {
+                    if (tmpimg.width > maxW || tmpimg.height > maxH) {
+                        var ratioW = tmpimg.width / maxW;
+                        var ratioH = tmpimg.height / maxH;
+                        if (ratioW > ratioH) {
+                            newW = maxW;
+                            newH = tmpimg.height / ratioW;
+                        } else {
+                            newH = maxH;
+                            newW = tmpimg.width / ratioH;
+                        }
+                    } else {
+                        newW = tmpimg.width;
+                        newH = tmpimg.height;
                     }
-                }else {
-                    newW=tmpimg.width;
-                    newH=tmpimg.height;
+                    can.width = newW;
+                    can.height = newH;
+                    ctx.drawImage(tmpimg, 0, 0, newW, newH);
                 }
-                can.width=newW;
-                can.height=newH;
-                ctx.drawImage(tmpimg,0,0,newW,newH);
             }
-        }
-    })(file);
-    reader.readAsDataURL(file);
-}
+        })(file);
+        reader.readAsDataURL(file);
+    }
+})
