d922c04b98e6f766cd0ed702b2d5c75328dced40
andymensional <andy.bialystock@gmail.com>
2013-03-09 22:05:22 +0100

    added some tools, cleaned filters and shit

--- js/picsma.main.js
+++ js/picsma.main.js
@@ -31,48 +31,49 @@ function loadPicture(source) {
 
 function updateFilterPreviews() {
     $('#preview').empty();
-    for (var i = 0; i < currentFilters.length; i++){
+    for (var i = 0; i < currentFilters.length; i++) {
         var newPreview = $('<canvas width="75" height="75" class="preview-image"></canvas>');
         newPreview.appendTo('#preview');
         newPreview[0].getContext('2d').drawImage($('#canvas')[0], -200, -200);
         picsma.filter.setCanvas(newPreview[0]);
         currentFilters[i].func();
         picsma.filter.setCanvas($('#canvas')[0]);
-        (function(){var currFunc = currentFilters[i].func;
-            newPreview.on("click",function(){
+        (function () {
+            var currFunc = currentFilters[i].func;
+            newPreview.on("click", function () {
                 currFunc($('#canvas')[0]);
-            });})()
-        }
+            });
+        })()
     }
+}
 
-    function stopDefault(e) {
-        e.stopPropagation();
-        e.preventDefault();
+function stopDefault(e) {
+    e.stopPropagation();
+    e.preventDefault();
+}
+
+function dropHandler(e) {
+    stopDefault(e);
+    uploadFile(e.dataTransfer.files[0]);
+}
+
+function uploadFile(file) {
+    if (!file.type.match(/image.*/)) return;
+    var reader = new FileReader();
+    reader.onerror = function (e) {
+        alert('Error: ' + e.target.error);
     }
-
-    function dropHandler(e) {
-        stopDefault(e);
-        uploadFile(e.dataTransfer.files[0]);
+    reader.onload = function (e) {
+        loadPicture(e.target.result);
     }
+    reader.readAsDataURL(file);
+}
 
-    function uploadFile(file){
-        if (!file.type.match(/image.*/)) return;
-        var reader = new FileReader();
-        reader.onerror = function (e) {
-            alert('Error: ' + e.target.error);
-        }
-        reader.onload = function (e) {
-            loadPicture(e.target.result);
-        }
-        reader.readAsDataURL(file);
-
-    }
-
-    $(function () {
-        var dropArea = document;
-        dropArea.addEventListener('dragenter', stopDefault, false);
-        dropArea.addEventListener('dragover', stopDefault, false);
-        dropArea.addEventListener('dragleave', stopDefault, false);
-        dropArea.addEventListener('drop', dropHandler, false);
-        loadPicture('img/ressources/example.jpg');
-    })
+$(function () {
+    var dropArea = document;
+    dropArea.addEventListener('dragenter', stopDefault, false);
+    dropArea.addEventListener('dragover', stopDefault, false);
+    dropArea.addEventListener('dragleave', stopDefault, false);
+    dropArea.addEventListener('drop', dropHandler, false);
+    loadPicture('img/ressources/example.jpg');
+})
